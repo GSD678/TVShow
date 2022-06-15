fe5ed48173f3f08dbcb8da158dc77d24997199e8
Karim <kimo_678@live.com>
2022-06-15 17:39:20 -0400

    add dummy data to tvcomponent.ts

--- src/app/tvcomponent/tvcomponent.component.ts
+++ src/app/tvcomponent/tvcomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITV } from '../itv';
 
 @Component({
   selector: 'app-tvcomponent',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tvcomponent.component.css']
 })
 export class TVComponentComponent implements OnInit {
+  current: ITV;
 
-  constructor() { }
+  constructor() {
+    this.current = {
+      name: "Barry",
+      status: "Runnning",
+      premiered: "2018-08-04",
+      network: "HBO",
+      image: "https://static.tvmaze.com/uploads/images/medium_portrait/403/1007647.jpg",
+      rating:"7.5",
+      runtime: 30,
+      time: "22:00",
+      days: "Sunday"
+    }
+  }
 
   ngOnInit(): void {
   }
