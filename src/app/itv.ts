f92ad7542a34489174fa2854b1279a9bb7eb803d
Jheel Goel <mailjheel@gmail.com>
2022-06-15 20:04:49 -0700

    display tv details

--- src/app/itv.ts
+++ src/app/itv.ts
@@ -1,3 +1,11 @@
 export interface ITV {
-
+    name: string,
+  status: string,
+  premiered: string,
+  network: string,
+  image: string,
+  rating: string,
+  runtime: number,
+    time: string,
+    days: string
 }
