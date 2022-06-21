9d690d9b13a01bed054a6a1bb044c763cb2bfcae
Karim <kimo_678@live.com>
2022-06-20 21:10:48 -0400

    updated tv service to accept user input

--- src/app/tvservice.service.ts
+++ src/app/tvservice.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
 import { ITVData } from './itvdata';
+import { map } from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class TVServiceService {
   constructor(private httpClient: HttpClient) { }
 
   getCurrentTVData(name: string) {
-    return this.httpClient.get<ITVData>(`https://api.tvmaze.com//singlesearch/shows?q=:${name}`);
+    return this.httpClient.get<ITVData>(`https://api.tvmaze.com//singlesearch/shows?q=:${name}`).pipe(map(data => this.transformToITV(data) ));
       }
 
   transformToITV(data: ITVData) {
